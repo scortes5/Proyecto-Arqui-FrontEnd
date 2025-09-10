e4ad045c88f88836551fa26fe9dc3b86e7f6da79
gaspar-mr05 <gaspar.magna@uc.cl>
2025-09-09 22:06:38 -0300

    feat: initial auth with auth0

---
+++ src/components/Auth/LoginButton.tsx
@@ -0,0 +1,21 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import { Button } from "../Button";
+
+const LoginButton = () => {
+  const { loginWithRedirect, isAuthenticated } = useAuth0();
+
+  return (
+    !isAuthenticated && (
+      <Button
+        onClick={() => {
+          loginWithRedirect();
+          console.log("aprietando");
+        }}
+      >
+        Iniciar sesion
+      </Button>
+    )
+  );
+};
+
+export default LoginButton;
