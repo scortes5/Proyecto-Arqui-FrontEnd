62e2bec91088dbff1fa3dcb1f03a8444382e19fa
gaspar-mr05 <gaspar.magna@uc.cl>
2025-09-17 18:10:11 -0300

    feat(auth0): add audience in auth provider params

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import { useAuth0 } from "@auth0/auth0-react";
+
 interface Props {}
 
 export const ProfilePage = ({}: Props) => {
-  return <div>Pagina para ver perfil</div>;
+  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
+  const [token, setToken] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      if (isAuthenticated) {
+        try {
+          const accessToken = await getAccessTokenSilently();
+          setToken(accessToken);
+          console.log("Access Token:", accessToken);
+        } catch (err) {
+          console.error("Error al obtener el token:", err);
+        }
+      }
+    };
+
+    fetchToken();
+  }, [isAuthenticated, getAccessTokenSilently]);
+
+  if (!isAuthenticated) return <div>Inicia sesión para ver tu perfil</div>;
+
+  return (
+    <div className="text-white">
+      <h1>Perfil de usuario</h1>
+      <pre>{JSON.stringify(user, null, 2)}</pre>
+      <h2>Access Token</h2>
+      <pre>{token}</pre>
+    </div>
+  );
 };
